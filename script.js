d4b15fce2288294c2bb4a64b1f26e8d0073985ef
Eric Takeda <sneric017@gmail.com>
2026-02-07 16:52:22 -0300

    Adicionei os arquivos

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarFormulario() { //Cria a função
+    document.getElementById("formulario").style.display = "block"; //Seleciona o elemento com Id formulário, o tornando visível
+    document.getElementById("iframe").style.display = "none"; //O que tem o Id Iframe desaparece
+}
+
+function mostrarIframe() { //Aqui o Iframe aparece e o formulário desaparece
+    document.getElementById("formulario").style.display = "none"; //O ".style.display" muda diretamente o estilo CSS
+    document.getElementById("iframe").style.display = "block";
+}
