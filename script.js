a7ab0278efe08f85c0663be7003707b16d72a0bd
Eric Takeda <sneric017@gmail.com>
2026-02-07 17:35:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ function mostrarFormulario() { //Cria a função
 function mostrarIframe() { //Aqui o Iframe aparece e o formulário desaparece
     document.getElementById("formulario").style.display = "none"; //O ".style.display" muda diretamente o estilo CSS
     document.getElementById("iframe").style.display = "block";
+
+}
+
+function botaoMenu(){
+  let menu = document.getElementById("menuConteudo");
+  if(menu.style.display === "block"){
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 }
